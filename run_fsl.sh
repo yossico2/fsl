d188047d043e598b78a121a1d83f9a7f232b9fa5
Yossi Cohen <yossico@rafael.co.il>
2025-12-17 08:26:18 +0200

    ref

--- run_fsl.sh
+++ run_fsl.sh
@@ -18,43 +18,54 @@ fi
 
 function main() {
 	local detach=""
-	local instance=0
+	local instance=""
 	local run_no_docker=0
 
+	# Source env.sh to get BUILD_TARGET_DIR
+	source "$(dirname "$0")/env.sh"
+	local fsldir="${BUILD_TARGET_DIR}"
+
 	# parse args in any order
-	for arg in "$@"; do
+	# 1. If -i/--instance or a positional instance is provided, it is used.
+	# 2. If not, and STATEFULSET_INDEX is defined, it is used.
+	# 3. Otherwise, it defaults to 0.
+	local arg_instance=""
+	for ((i=1; i<=$#; i++)); do
+		arg="${!i}"
 		if [[ "$arg" == "-d" ]]; then
 			detach="-d"
 		elif [[ "$arg" == "-nd" ]]; then
 			run_no_docker=1
-		elif [[ "$arg" =~ ^[0-9]+$ ]]; then
-			instance="$arg"
+		elif [[ "$arg" == "-i" || "$arg" == "--instance" ]]; then
+			next_idx=$((i+1))
+			if [[ $next_idx -le $# ]]; then
+				arg_instance="${!next_idx}"
+			fi
+		elif [[ "$arg" =~ ^[0-9]+$ && -z "$arg_instance" ]]; then
+			arg_instance="$arg"
 		fi
 	done
+
+	# Determine instance priority
+	if [[ -n "$arg_instance" ]]; then
+		instance="$arg_instance"
+	elif [[ -n "$STATEFULSET_INDEX" ]]; then
+		instance="$STATEFULSET_INDEX"
+	else
+		instance=0
+	fi
 
 	# Check NO_DOCKER env var
 	if [[ "${NO_DOCKER}" == "1" ]]; then
 		run_no_docker=1
 	fi
 
+	local prev_pids=
 	if [[ ${run_no_docker} -eq 1 ]]; then
 		logdir="${HOME}/dev/logs"
 		mkdir -p "${logdir}"
-
-		local fsldir=
-		if [[ "${ABSOLUTE_FSL_RUN}" == "true" ]]; then
-			fsldir="${HOME}/dev/elar/elar_fsl/build/linux/debug"
-		else
-			# source env.sh to get BUILD_TARGET_DIR
-			source "$(dirname "$0")/env.sh"
-			fsldir="${BUILD_TARGET_DIR}"
-		fi
-
-		cd "${fsldir}" || exit 1
-
-		# kill all previous fsl for this instance
-		# use pgrep to find and kill all fsl processes for this instance
-		local prev_pids=
+		# Kill all previous fsl for this instance
+		# Use pgrep to find and kill all fsl processes for this instance
 		prev_pids=$(pgrep -f "./fsl ${instance}")
 		echo "Previous fsl instances for instance ${instance}: ${prev_pids}"
 		if [[ -n "${prev_pids}" ]]; then
@@ -62,13 +73,12 @@ function main() {
 				kill "${pid}"
 			done
 		fi
-
+		cd "${fsldir}" || exit 1
 		nohup ./fsl "${instance}" >"${logdir}/fsl-${instance}.log" 2>&1 &
 		if [[ $? -ne 0 ]]; then
 			echo "Error: Failed to start fsl in background" >&2
 			exit 1
 		fi
-
 		cd - &>/dev/null || exit 1
 		echo "Started fsl instance ${instance} in background. Output: ${logdir}/fsl-${instance}.log" >&2
 	else
